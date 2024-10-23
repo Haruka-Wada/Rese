e16367aa71678baed545523987bc38e063cf2682
haruka wada <022.wadaha@gmail.com>
2024-10-23 16:36:02 +0900

    menu画面の作成

---
+++ .history/src/resources/views/layouts/app.blade_20241023151452.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rese</title>
+    <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    @yield('css')
+</head>
+<body>
+    <header>
+        <div class="header">
+            <a href="#modal-container"></a>
+            <h1 class="header__logo">Rese</h1>
+        </div>
+    </header>
+    <main>
+        @yield('main')
+    </main>
+</body>
+</html>
---
+++ .history/src/resources/views/layouts/app.blade_20241023152519.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rese</title>
+    <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    @yield('css')
+</head>
+
+<body>
+    <header>
+        <div class="header">
+            <a href="#modal-container" class="header__logo">Rese</a>
+        </div>
+    </header>
+    <main>
+        @yield('main')
+    </main>
+
+    <div id="modal-container">
+        <a href="#!" class="modal-overlay"></a>
+        <div class="modal__inner">
+            <a href="#" ></a>
+            <div class="modal__content">
+                <a href="/">HOME</a>
+                <form action="/logout" method="post">
+                    @csrf
+                    <button>Logout</button>
+                </form>
+                <a href="/">Mypage</a>
+            </div>
+
+        </div>
+    </div>
+</body>
+
+</html>
