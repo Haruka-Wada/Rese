b4b2d099c55561ac8f19b09a81d25bedc6c69543
haruka wada <022.wadaha@gmail.com>
2024-11-06 19:31:00 +0900

    予約フォームの修正、予約機能追加

---
+++ .history/src/resources/js/rese_20241106163535.js
@@ -0,0 +1,26 @@
+    $(function() {
+        let like = $('.like-toggle');
+        let likeShopId;
+        like.on('click', function() {
+            let $this = $(this);
+            likeShopId = $this.data('shop-id');
+            $.ajax({
+                    headers: {
+                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                    },
+                    url: '/favorite',
+                    method: 'POST',
+                    data: {
+                        'shop_id': likeShopId
+                    },
+                })
+
+                .done(function(data) {
+                    $this.toggleClass('liked');
+                })
+                .fail(function() {
+                    console.log('fail');
+                })
+        })
+    })
+</script>
