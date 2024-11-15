5faab90318ddd5d97db9148483cb155843c5aacd
haruka wada <022.wadaha@gmail.com>
2024-11-15 15:29:30 +0900

    ハンバーガーメニュー編集

---
+++ .history/src/resources/views/auth/thanks.blade_20241115150601.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/thanks_done.css') }}">
+@endsection
+
+@section('main')
+    <div class="main__container">
+        <div class="main__text">
+            <p>会員登録ありがとうございます</p>
+        </div>
+        <div class="main__login">
+            <a class="main__login-button" href="./login">ログインする</a>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ .history/src/resources/views/layouts/app.blade_20241115130503.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="csrf-token" content="{{ csrf_token()}}">
+    <title>Rese</title>
+    <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    @yield('css')
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+</head>
+
+<body>
+
+    <header>
+        <div class="header__wrapper">
+            <nav class="header__hamburger">
+
+            </nav>
+            <h1 class="header__logo">Rese</h1>
+            @yield('search')
+        </div>
+
+        <div class="menu__overlay">
+            
+        </div>
+    </header>
+    <main class="main">
+        <div class="main__overlay">
+            @yield('main')
+        </div>
+    </main>
+
+    <script type="text/javascript">
+        $(function() {
+            $('.header__hamburger').click(function() {
+                $('.header__overlay,.header__wrapper,.header__hamburger,.header__logo,.header__search,.menu__overlay,.main__overlay').toggleClass('active');
+            });
+        });
+    </script>
+</body>
+
+</html>
