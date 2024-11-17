84f87f51e6f555f969297f779be32fc23f3918bf
haruka wada <022.wadaha@gmail.com>
2024-11-17 17:46:36 +0900

    バリデーション機能追加

---
+++ .history/src/resources/views/auth/login.blade_20241117164800.php
@@ -0,0 +1,44 @@
+@extends('layouts.app')
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/auth/login.css') }}">
+@endsection
+
+@section('main')
+<div class="form__container">
+    <div class="form__title">
+        <p class="form__title-text">
+            Login
+        </p>
+    </div>
+    <div class="form__content">
+        <form class="form" action="/login" method="post">
+            @csrf
+            <div class="form-item">
+                <div class="form-icon">
+                    <img src="{{ asset('img/mail.png') }}" alt="">
+                </div>
+                <div class="form-text">
+                    <input type="email" name="email" value="{{ old('email') }}" placeholder="Email">
+                </div>
+            </div>
+            <div class="form__error">
+                @error('email')
+                {{ $message }}
+                @enderror
+            </div>
+            <div class="form-item">
+                <div class="form-icon">
+                    <img src="{{ asset('img/key.png') }}" alt="">
+                </div>
+                <div class="form-text">
+                    <input type="password" name="password" placeholder="Password">
+                </div>
+            </div>
+            <div class="form-button">
+                <button>ログイン</button>
+            </div>
+        </form>
+    </div>
+</div>
+@endsection
