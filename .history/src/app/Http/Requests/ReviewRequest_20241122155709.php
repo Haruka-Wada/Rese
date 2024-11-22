c8caada8f1ae02e5b6560e5d8471f954579a68e0
haruka wada <022.wadaha@gmail.com>
2024-11-22 17:43:20 +0900

    レビューページのレスポンシブ画面編集

---
+++ .history/src/app/Http/Requests/ReviewRequest_20241122155709.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests;
+
+use GuzzleHttp\Psr7\Message;
+use Illuminate\Foundation\Http\FormRequest;
+
+class ReviewRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'rating' => 'required',
+            'comment' => 'required'
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'rating.required' => '総合評価は必ず入力してください',
+            'comment.required' => 'コメントは必ず入力してください'
+        ]
+    }
+}
